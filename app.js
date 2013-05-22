877c9b029dd201bd52f35d316ad1f2b1e7c01538
brentcobb <brent.tm.cobb@gmail.com>
2013-05-22 16:05:19 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+angular.module('Twitter', []).controller('MainCtrl', function($scope, $http) {
+  $scope.search = function(query) {
+    $http.jsonp('http://search.twitter.com/search.json?q=' + query + '&callback=JSON_CALLBACK')
+      .success(function(data) {
+        $scope.items = data.results;
+      });
+  };
+});
